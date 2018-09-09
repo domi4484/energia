9a7e64318f286cf555ac3d51c126f7302b50d478
Damiano <lombardi.damiano@gmail.com>
2018-09-09 10:41:39 +0200

    ...

--- CSVImporterSettings.cpp
+++ CSVImporterSettings.cpp
@@ -1,7 +1,39 @@
 
+// File includes ------------------------------------------
 #include "CSVImporterSettings.h"
 
-CSVImporterSettings::CSVImporterSettings(QObject *parent) : QObject(parent)
+CSVImporterSettings::CSVImporterSettings(QObject *parent)
+  : QObject(parent)
+  , m_QList_Correspondances()
+{
+  m_QList_Correspondances.append(Correspondance("Time", "", true));
+  m_QList_Correspondances.append(Correspondance("Solare L1 (kWh) day", "", true));
+  m_QList_Correspondances.append(Correspondance("Solare L2 (kWh) day", "", true));
+  m_QList_Correspondances.append(Correspondance("Solare L3 (kWh) day", "", true));
+
+  m_QList_Correspondances.append(Correspondance("Appartamento1 L1 (kWh) day", "", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento1 L2 (kWh) day", "", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento1 L3 (kWh) day", "", true));
+
+  m_QList_Correspondances.append(Correspondance("Appartamento2 L1 (kWh) day", "", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento2 L2 (kWh) day", "", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento2 L3 (kWh) day", "", true));
+
+  m_QList_Correspondances.append(Correspondance("Appartamento3 L1 (kWh) day", "", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento3 L2 (kWh) day", "", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento3 L3 (kWh) day", "", true));
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+void CSVImporterSettings::loadFromFile(const QString &filename)
+{
+
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+void CSVImporterSettings::saveToFile(const QString &filename)
 {
 
 }
--- CSVImporterSettings.h
+++ CSVImporterSettings.h
@@ -1,17 +1,42 @@
 #ifndef CSVIMPORTERSETTINGS_H
 #define CSVIMPORTERSETTINGS_H
 
+// Qt includes --------------------------------------------
 #include <QObject>
 
 class CSVImporterSettings : public QObject
 {
     Q_OBJECT
+
 public:
-    explicit CSVImporterSettings(QObject *parent = nullptr);
 
-signals:
+  explicit CSVImporterSettings(QObject *parent = nullptr);
 
-public slots:
+  void loadFromFile(const QString &filename);
+  void saveToFile(const QString &filename);
+
+private:
+
+  class Correspondance
+  {
+  public:
+
+    Correspondance(const QString &csvHeader,
+                   const QString &databaseColumnName,
+                   bool enabled)
+      : m_CSVHeader(csvHeader)
+      , m_DatabaseColumnName(databaseColumnName)
+      , m_Enabled(enabled)
+    {}
+
+    QString m_CSVHeader;
+    QString m_DatabaseColumnName;
+    bool m_Enabled;
+
+  }; // Correspondance
+
+  QList<Correspondance> m_QList_Correspondances;
+
 };
 
 #endif // CSVIMPORTERSETTINGS_H
