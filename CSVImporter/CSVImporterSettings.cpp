0ae51aed32dee2e41b2f04cc15f77d250e0a620b
Damiano <lombardi.damiano@gmail.com>
2018-10-02 20:14:39 +0200

    ...

--- CSVImporter/CSVImporterSettings.cpp
+++ CSVImporter/CSVImporterSettings.cpp
@@ -6,35 +6,35 @@ CSVImporterSettings::CSVImporterSettings(QObject *parent)
   : QObject(parent)
   , m_QList_Correspondances()
 {
-  m_QList_Correspondances.append(Correspondance("Time", "", true));
+  m_QList_Correspondances.append(Correspondance("Time", "timestamp", true));
 
-  m_QList_Correspondances.append(Correspondance("Solare L1 (kWh) night", "solare-l1", true));
-  m_QList_Correspondances.append(Correspondance("Solare L2 (kWh) night", "solare-l2", true));
-  m_QList_Correspondances.append(Correspondance("Solare L3 (kWh) night", "solare-l3", true));
+  m_QList_Correspondances.append(Correspondance("Solare L1 (kWh) night", "solare_l1", true));
+  m_QList_Correspondances.append(Correspondance("Solare L2 (kWh) night", "solare_l2", true));
+  m_QList_Correspondances.append(Correspondance("Solare L3 (kWh) night", "solare_l3", true));
 
-  m_QList_Correspondances.append(Correspondance("Appartamento1 L1 (kWh) night", "casa15-appartamento1-l1", true));
-  m_QList_Correspondances.append(Correspondance("Appartamento1 L2 (kWh) night", "casa15-appartamento1-l2", true));
-  m_QList_Correspondances.append(Correspondance("Appartamento1 L3 (kWh) night", "casa15-appartamento1-l3", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento1 L1 (kWh) night", "casa15_appartamento1_l1", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento1 L2 (kWh) night", "casa15_appartamento1_l2", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento1 L3 (kWh) night", "casa15_appartamento1_l3", true));
 
-  m_QList_Correspondances.append(Correspondance("Appartamento2 L1 (kWh) night", "casa15-appartamento2-l1", true));
-  m_QList_Correspondances.append(Correspondance("Appartamento2 L2 (kWh) night", "casa15-appartamento2-l2", true));
-  m_QList_Correspondances.append(Correspondance("Appartamento2 L3 (kWh) night", "casa15-appartamento2-l3", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento2 L1 (kWh) night", "casa15_appartamento2_l1", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento2 L2 (kWh) night", "casa15_appartamento2_l2", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento2 L3 (kWh) night", "casa15_appartamento2_l3", true));
 
-  m_QList_Correspondances.append(Correspondance("Appartamento3 L1 (kWh) night", "casa15-appartamento3-l1", true));
-  m_QList_Correspondances.append(Correspondance("Appartamento3 L2 (kWh) night", "casa15-appartamento3-l2", true));
-  m_QList_Correspondances.append(Correspondance("Appartamento3 L3 (kWh) night", "casa15-appartamento3-l3", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento3 L1 (kWh) night", "casa15_appartamento3_l1", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento3 L2 (kWh) night", "casa15_appartamento3_l2", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento3 L3 (kWh) night", "casa15_appartamento3_l3", true));
 
-  m_QList_Correspondances.append(Correspondance("Appartamento1 L1 (kWh) night", "casa17-appartamento1-l1", true));
-  m_QList_Correspondances.append(Correspondance("Appartamento1 L2 (kWh) night", "casa17-appartamento1-l2", true));
-  m_QList_Correspondances.append(Correspondance("Appartamento1 L3 (kWh) night", "casa17-appartamento1-l3", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento1 L1 (kWh) night", "casa17_appartamento1_l1", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento1 L2 (kWh) night", "casa17_appartamento1_l2", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento1 L3 (kWh) night", "casa17_appartamento1_l3", true));
 
-  m_QList_Correspondances.append(Correspondance("Appartamento2 L1 (kWh) night", "casa17-appartamento2-l1", true));
-  m_QList_Correspondances.append(Correspondance("Appartamento2 L2 (kWh) night", "casa17-appartamento2-l2", true));
-  m_QList_Correspondances.append(Correspondance("Appartamento2 L3 (kWh) night", "casa17-appartamento2-l3", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento2 L1 (kWh) night", "casa17_appartamento2_l1", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento2 L2 (kWh) night", "casa17_appartamento2_l2", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento2 L3 (kWh) night", "casa17_appartamento2_l3", true));
 
-  m_QList_Correspondances.append(Correspondance("Appartamento3 L1 (kWh) night", "casa17-appartamento3-l1", true));
-  m_QList_Correspondances.append(Correspondance("Appartamento3 L2 (kWh) night", "casa17-appartamento3-l2", true));
-  m_QList_Correspondances.append(Correspondance("Appartamento3 L3 (kWh) night", "casa17-appartamento3-l3", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento3 L1 (kWh) night", "casa17_appartamento3_l1", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento3 L2 (kWh) night", "casa17_appartamento3_l2", true));
+  m_QList_Correspondances.append(Correspondance("Appartamento3 L3 (kWh) night", "casa17_appartamento3_l3", true));
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------
