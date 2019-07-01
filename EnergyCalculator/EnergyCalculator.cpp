d43f28640702cf011307dbda58af32b4738e0d2b
Damiano Lombardi <damiano@lowerspot.com>
2019-07-01 11:05:43 +0200

    ...

--- EnergyCalculator/EnergyCalculator.cpp
+++ EnergyCalculator/EnergyCalculator.cpp
@@ -3,6 +3,7 @@
 #include "EnergyCalculator.h"
 
 // Qt includes --------------------------------------------
+#include <QDebug>
 #include <QVariantMap>
 
 //-----------------------------------------------------------------------------------------------------------------------------
@@ -22,40 +23,102 @@ EnergyCalculator::Result EnergyCalculator::DistributeEnergy(const QList<QVariant
   // Result entities based on configuration
   Result result;
 
-  // Energy production
-  result.m_EnergyProductionEntity.m_Name = m_Configuration.m_EnergyProductionEntity.m_Name;
-
-  // Energy consumption
-  foreach(const EnergyConsumptionEntity &energyConsumptionEntity, m_Configuration.m_QList_EnergyConsumptionEntity)
+  // Production/consumption
   {
-    Result::EnergyConsumptionEntity resultEnergyConsumptionEntity;
-    resultEnergyConsumptionEntity.m_Name = energyConsumptionEntity.m_Name;
-    result.m_QMap_EnergyConsumptionEntity.insert(resultEnergyConsumptionEntity.m_Name, resultEnergyConsumptionEntity);
-  }
+    // Energy production
+    result.m_EnergyProductionEntity.m_Name = m_Configuration.m_EnergyProductionEntity.m_Name;
 
-  // Foreach row
-  foreach(const QVariantMap & qVariantMap, qList_QVariantMap_Rows)
-  {
-    // Total production
-    result.m_EnergyProductionEntity.m_L1.m_Production += qVariantMap.value(m_Configuration.m_EnergyProductionEntity.m_KeyL1).toDouble();
-    result.m_EnergyProductionEntity.m_L2.m_Production += qVariantMap.value(m_Configuration.m_EnergyProductionEntity.m_KeyL2).toDouble();
-    result.m_EnergyProductionEntity.m_L2.m_Production += qVariantMap.value(m_Configuration.m_EnergyProductionEntity.m_KeyL3).toDouble();
-    result.m_EnergyProductionEntity.m_Total.m_Production = result.m_EnergyProductionEntity.m_Total.m_Production
-                                                         + result.m_EnergyProductionEntity.m_L1.m_Production
-                                                         + result.m_EnergyProductionEntity.m_L2.m_Production
-                                                         + result.m_EnergyProductionEntity.m_L3.m_Production;
-
-    // Total consumption
+    // Energy consumption
     foreach(const EnergyConsumptionEntity &energyConsumptionEntity, m_Configuration.m_QList_EnergyConsumptionEntity)
     {
-      result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_L1.m_Consumption += qVariantMap.value(energyConsumptionEntity.m_KeyL1).toDouble();
-      result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_L2.m_Consumption += qVariantMap.value(energyConsumptionEntity.m_KeyL2).toDouble();
-      result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_L3.m_Consumption += qVariantMap.value(energyConsumptionEntity.m_KeyL3).toDouble();
-      result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_Total.m_Consumption = result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_Total.m_Consumption
-                                                                                                  + result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_L1.m_Consumption
-                                                                                                  + result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_L2.m_Consumption
-                                                                                                  + result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_L3.m_Consumption;
+      Result::EnergyConsumptionEntity resultEnergyConsumptionEntity;
+      resultEnergyConsumptionEntity.m_Name = energyConsumptionEntity.m_Name;
+      result.m_QMap_EnergyConsumptionEntity.insert(resultEnergyConsumptionEntity.m_Name, resultEnergyConsumptionEntity);
     }
+
+    // Foreach row
+    foreach(const QVariantMap & qVariantMap, qList_QVariantMap_Rows)
+    {
+      // Total production
+      result.m_EnergyProductionEntity.m_L1.m_Production += qVariantMap.value(m_Configuration.m_EnergyProductionEntity.m_KeyL1).toDouble();
+      result.m_EnergyProductionEntity.m_L2.m_Production += qVariantMap.value(m_Configuration.m_EnergyProductionEntity.m_KeyL2).toDouble();
+      result.m_EnergyProductionEntity.m_L3.m_Production += qVariantMap.value(m_Configuration.m_EnergyProductionEntity.m_KeyL3).toDouble();
+
+      // Total consumption
+      foreach(const EnergyConsumptionEntity &energyConsumptionEntity, m_Configuration.m_QList_EnergyConsumptionEntity)
+      {
+        result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_L1.m_Consumption += qVariantMap.value(energyConsumptionEntity.m_KeyL1).toDouble();
+        result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_L2.m_Consumption += qVariantMap.value(energyConsumptionEntity.m_KeyL2).toDouble();
+        result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_L3.m_Consumption += qVariantMap.value(energyConsumptionEntity.m_KeyL3).toDouble();
+        result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_Total.m_Consumption = result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_Total.m_Consumption
+            + result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_L1.m_Consumption
+            + result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_L2.m_Consumption
+            + result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_L3.m_Consumption;
+      }
+    }
+
+    // Totals
+    {
+      result.m_EnergyProductionEntity.m_Total.m_Production = result.m_EnergyProductionEntity.m_L1.m_Production
+                                                           + result.m_EnergyProductionEntity.m_L2.m_Production
+                                                           + result.m_EnergyProductionEntity.m_L3.m_Production;
+
+      foreach(const EnergyConsumptionEntity &energyConsumptionEntity, m_Configuration.m_QList_EnergyConsumptionEntity)
+        result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_Total.m_Consumption = result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_L1.m_Consumption
+                                                                                                    + result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_L2.m_Consumption
+                                                                                                    + result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_L3.m_Consumption;
+    }
+  }
+
+  // Self consumption
+  {
+    // Get priority groups
+    QList<QList<EnergyConsumptionEntity> > qList_EnergyConsumptionGroups;
+    {
+      QList<int> qList_Priorities;
+      foreach(const EnergyConsumptionEntity &energyConsumptionEntity, m_Configuration.m_QList_EnergyConsumptionEntity)
+      {
+        if(qList_Priorities.contains(energyConsumptionEntity.m_Priority) == false)
+          qList_Priorities.append(energyConsumptionEntity.m_Priority);
+      }
+      std::sort(qList_Priorities.begin(), qList_Priorities.end());
+
+      foreach (int priority, qList_Priorities)
+      {
+        QList<EnergyConsumptionEntity> qList_EnergyConsumptionGroup;
+        foreach(const EnergyConsumptionEntity &energyConsumptionEntity, m_Configuration.m_QList_EnergyConsumptionEntity)
+        {
+          if(energyConsumptionEntity.m_Priority == priority)
+            qList_EnergyConsumptionGroup.append(energyConsumptionEntity);
+        }
+        if(qList_EnergyConsumptionGroup.isEmpty() == false)
+          qList_EnergyConsumptionGroups.append(qList_EnergyConsumptionGroup);
+      }
+    }
+
+    // Foreach row
+    foreach(const QVariantMap & qVariantMap, qList_QVariantMap_Rows)
+    {
+      // Total production
+      double L1_Production = qVariantMap.value(m_Configuration.m_EnergyProductionEntity.m_KeyL1).toDouble();
+      double L2_Production = qVariantMap.value(m_Configuration.m_EnergyProductionEntity.m_KeyL2).toDouble();
+      double L3_Production = qVariantMap.value(m_Configuration.m_EnergyProductionEntity.m_KeyL3).toDouble();
+
+      // For each group ordered by priority
+      foreach (const QList<EnergyConsumptionEntity> & qList_EnergyConsumptionGroup, qList_EnergyConsumptionGroups)
+      {
+//        foreach(const EnergyConsumptionEntity &energyConsumptionEntity, priorityGroups)
+//        {
+//          result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_L1.m_Consumption += qVariantMap.value(energyConsumptionEntity.m_KeyL1).toDouble();
+//          result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_L2.m_Consumption += qVariantMap.value(energyConsumptionEntity.m_KeyL2).toDouble();
+//          result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_L3.m_Consumption += qVariantMap.value(energyConsumptionEntity.m_KeyL3).toDouble();
+//          result.m_QMap_EnergyConsumptionEntity[energyConsumptionEntity.m_Name].m_Total.m_Consumption = result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_Total.m_Consumption
+//              + result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_L1.m_Consumption
+//              + result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_L2.m_Consumption
+//              + result.m_QMap_EnergyConsumptionEntity.value(energyConsumptionEntity.m_Name).m_L3.m_Consumption;
+//        }
+      } // foreach qList_EnergyConsumptionGroup
+    } // foreach row
   }
 
   return result;
